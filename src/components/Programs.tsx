973690e6e5d34cf31c4c9fd108d053448a2893c5
bozzhik <bozzhik@ya.ru>
2024-04-07 02:22:33 +0300

    refactor body of `Programs` block

--- src/components/Programs.tsx
+++ src/components/Programs.tsx
@@ -11,6 +11,8 @@ interface Program {
   slug: {current: string}
 }
 
+export const revalidate = 30
+
 const getData = async (): Promise<Program[]> => {
   const query = `
     *[_type == 'program'] {
@@ -30,18 +32,18 @@ const Program = async () => {
   const programs: Program[] = await getData()
 
   return (
-    <section className="flex flex-row flex-wrap items-start gap-5 p-5 m-5 mt-20 border-2 border-custom-teal/25">
+    <section className="grid grid-cols-4 xl:grid-cols-3 sm:grid-cols-1 items-start gap-5 p-5 m-5 mt-20 border-2 border-custom-teal/25">
       {programs.map((program, idx) => (
-        <Link key={idx} href={`/program/${program.slug.current}`} className="flex flex-col w-[24%] gap-5 p-5 border-2 border-custom-teal group">
-          {program.images.map((image, index) => (
-            <div key={index} className="w-full h-[15vw] relative self-center">
-              <Image src={urlForImage(image).url()} className="object-cover" fill={true} alt={`program ${index}`} />
+        <Link key={idx} href={`/program/${program.slug.current}`} className="flex flex-col justify-between h-[500px] w-full gap-5 p-5 border-2 border-custom-teal group">
+          {program.images.length > 0 && (
+            <div className="w-full h-[250px] relative self-center">
+              <Image src={urlForImage(program.images[0]).url()} className="object-cover" fill={true} alt={`program 0`} />
             </div>
-          ))}
+          )}
 
-          <div className="space-y-2 line-clamp-4">
-            <h1 className="text-2xl font-medium">{program.name}</h1>
-            <h2>{program.description}</h2>
+          <div className="space-y-2">
+            <h1 className="text-2xl font-medium ">{program.name}</h1>
+            <h2 className="line-clamp-3">{program.description}</h2>
           </div>
           <button title="Подробнее" className="w-full py-2 text-white duration-300 bg-custom-teal group-hover:bg-custom-teal/85">
             Подробнее
