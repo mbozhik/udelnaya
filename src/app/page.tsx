adca574322be2102c369f6fd31b526c9871b9fe2
bozzhik <bozzhik@ya.ru>
2024-04-13 02:38:28 +0300

    init `Slider` index block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,9 @@
-import Link from 'next/link'
-
-import Stocks from '@/components/Stocks'
+import Slider from '#/App/index/Slider'
 
 export default function Home() {
   return (
     <>
-      <Link prefetch={false} href={'/program/'} className="duration-200 block w-fit mt-5 mx-auto hover:text-custom-teal">
-        К программам
-      </Link>
-      <Stocks />
+      <Slider />
     </>
   )
 }
