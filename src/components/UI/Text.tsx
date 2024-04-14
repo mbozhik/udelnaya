98a7176e5b5cc383b900ca4c4b66dc30b9b91b5e
bozzhik <bozzhik@ya.ru>
2024-04-15 00:19:11 +0300

    init `Text` UI component

---
+++ src/components/UI/Text.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  type?: 'heading' | 'text'
+  text?: string
+  classes?: string
+}
+
+const Text: React.FC<Props> = ({type = 'text', text, classes}) =>
+  // prettier-ignore
+  type === 'heading' ? (
+    <h1 className={`text-4xl font-book uppercase sm:text-2xl text-custom-blue ${classes}`} dangerouslySetInnerHTML={{__html: text || ''}} />
+  ) : (
+    <p className={`text-base sm:text-sm ${classes}`} dangerouslySetInnerHTML={{__html: text || ''}} />
+  )
+
+export default Text
