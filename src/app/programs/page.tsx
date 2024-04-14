cf4da795bbbcca2bfde3c872832d30f90246457b
bozzhik <bozzhik@ya.ru>
2024-04-15 02:50:20 +0300

    fix `failed` vercel build due `program` page

---
+++ src/app/programs/page.tsx
@@ -0,0 +1,10 @@
+import Container from '#/UI/Container'
+import Programs from '##/index/Programs'
+
+export default function Program() {
+  return (
+    <Container>
+      <Programs />
+    </Container>
+  )
+}
--- src/components/UI/Container.tsx
+++ src/components/UI/Container.tsx
@@ -1,7 +1,8 @@
-export default function Container({
-  children,
-}: Readonly<{
+interface Props {
   children: React.ReactNode
-}>) {
-  return <main className="w-[70%] xl:w-[60%] sm:w-[90%] mx-auto pb-[15vh]">{children}</main>
+  padding?: boolean
+}
+
+export default function Container({padding = true, children}: Props) {
+  return <main className={`w-[70%] xl:w-[60%] sm:w-[90%] mx-auto pb-[15vh] ${padding ? 'pt-14' : ''}`}>{children}</main>
 }
