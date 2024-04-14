479c610a5537225dbc6b58fc3d527d3e824bb5cc
bozzhik <bozzhik@ya.ru>
2024-04-15 02:21:06 +0300

    remake `Button` UI component

--- src/components/UI/Button.tsx
+++ src/components/UI/Button.tsx
@@ -1,34 +1,48 @@
+import {cn} from '@/lib/utils'
 import Link from 'next/link'
 
-interface Button {
+interface Props {
+  type: 'link' | 'button'
   text: string
-  type?: 'link' | 'button'
   variant?: 'primary' | 'secondary'
-  href: string
+  size: 'md' | 'lg'
+  adavanced_hover?: boolean
+  href?: string
   blank?: boolean
   classes?: string
 }
 
 export const buttonVariants = {
-  default: 'px-5 sm:px-4 py-1.5 text-xl xl:text-lg sm:text-base border-2 border-transparent duration-300 cursor-pointer tracking-normal',
-  primary: 'text-white bg-custom-teal hover:bg-custom-gray',
-  secondary: 'text-custom-teal !border-custom-teal hover:!border-custom-gray hover:text-custom-gray',
+  default: {
+    styles: 'px-5 sm:px-4 py-1.5 border-2 border-custom-teal duration-300 cursor-pointer tracking-normal',
+    hover: 'group-hover:bg-custom-teal/85 group-hover:border-transparent',
+  },
+  primary: {
+    default: 'text-white bg-custom-teal',
+    hover: 'hover:bg-custom-gray hover:border-custom-gray',
+  },
+  secondary: {
+    default: 'text-custom-teal',
+    hover: 'hover:border-custom-gray hover:text-custom-gray',
+  },
 }
 
-const Button: React.FC<Button> = ({text, type = 'link', variant = 'primary', href, blank, classes}) => {
-  const buttonStyles = `${buttonVariants[variant] || buttonVariants['primary']} ${buttonVariants.default}  ${classes}`
+const Button: React.FC<Props> = ({type, text, variant = 'primary', size, adavanced_hover = false, href, blank, classes}) => {
+  const buttonStyles = `
+  ${buttonVariants.default.styles} ${buttonVariants[variant].default} 
+  ${adavanced_hover ? buttonVariants[variant].hover : buttonVariants.default.hover} 
+  ${size === 'lg' && 'text-xl xl:text-lg sm:text-base'} ${cn(classes)}
+  `
 
-  return (
-    <>
-      {type === 'link' ? (
-        <Link href={href} target={blank && '_blank'} className={buttonStyles}>
-          {text}
-        </Link>
-      ) : (
-        <button className={buttonStyles}>{text}</button>
-      )}
-    </>
-  )
+  if (type === 'link') {
+    return (
+      <Link href={href} target={blank && '_blank'} className={buttonStyles}>
+        {text}
+      </Link>
+    )
+  } else if (type === 'button') {
+    return <button className={buttonStyles}>{text}</button>
+  }
 }
 
 export default Button
