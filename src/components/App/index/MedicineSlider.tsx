a06b7eb0e3b16c38f980d2dc080bf4a02a79d15f
bozzhik <bozzhik@ya.ru>
2024-04-15 17:52:44 +0300

    install `bozzhik/is-mobile` to adaptive `Medicine` on mobile

--- src/components/App/index/MedicineSlider.tsx
+++ src/components/App/index/MedicineSlider.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import isMobile from '@bozzhik/is-mobile'
+
 import Link from 'next/link'
 import Image from 'next/image'
 import {Swiper, SwiperSlide} from 'swiper/react'
@@ -23,23 +25,29 @@ interface SliderProps {
 }
 
 const Slider: React.FC<SliderProps> = ({sliderData, classes}) => {
-  return (
-    <Swiper data-section="stocks-index" className={classes} slidesPerView={2} spaceBetween={10} loop={true} speed={1000} navigation={true} modules={[Navigation]}>
-      {sliderData.map((slide, index) => (
-        <SwiperSlide key={index}>
-          <Link className="relative grid w-full h-full overflow-hidden place-items-center group" href={`/procedure/${slide.slug}`}>
-            <Image quality={100} priority={true} className="object-cover s-full group-hover:scale-[103%] duration-500" src={slide.imageUrl} fill={true} alt={`акция ${index + 1}`} />
+  const slides = sliderData.map((slide, index) => (
+    <SwiperSlide key={index}>
+      <Link className="relative grid w-full h-full overflow-hidden place-items-center group" href={`/procedure/${slide.slug}`}>
+        <Image quality={100} priority={true} className="object-cover s-full group-hover:scale-[103%] duration-500" src={slide.imageUrl} fill={true} alt={`акция ${index + 1}`} />
 
-            <div className="absolute inset-0 flex flex-col justify-end bg-black bg-opacity-45">
-              <div className="p-5 w-[92%] space-y-2 text-white sm:text-center">
-                {slide.special_offer && <mark>АКЦИЯ</mark>}
-                <Heading type="title" text={slide.name} />
-                <Heading type="caption" classes="line-clamp-2" text={slide.description} />
-              </div>
-            </div>
-          </Link>
-        </SwiperSlide>
-      ))}
+        <div className="absolute inset-0 flex flex-col justify-end bg-black bg-opacity-45">
+          <div className="p-5 w-[92%] space-y-2 sm:space-y-1 text-white">
+            {slide.special_offer && <mark>АКЦИЯ</mark>}
+            <Heading type="title" text={slide.name} />
+            <Heading type="caption" classes="line-clamp-2 sm:hidden" text={slide.description} />
+          </div>
+        </div>
+      </Link>
+    </SwiperSlide>
+  ))
+
+  return !isMobile ? (
+    <Swiper data-section="medicine-index" className={classes} slidesPerView={2} spaceBetween={10} loop={true} speed={1000} navigation={true} modules={[Navigation]}>
+      {slides}
+    </Swiper>
+  ) : (
+    <Swiper data-section="medicine-mobile-index" className={classes} slidesPerView={1} spaceBetween={10} loop={true} speed={1000} navigation={true} modules={[Navigation]}>
+      {slides}
     </Swiper>
   )
 }
