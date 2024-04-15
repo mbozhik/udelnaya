9ccee071e31365e54a56ef52d9938e9d52208f4c
bozzhik <bozzhik@ya.ru>
2024-04-15 17:58:39 +0300

    add `hover` effect for cards from `Programs` block

--- src/components/App/index/Programs.tsx
+++ src/components/App/index/Programs.tsx
@@ -37,28 +37,28 @@ const Program = async () => {
     return <mark>Произошла ошибка при получении данных!</mark>
   }
 
+  const slides = programs.map((program, idx) => (
+    <Link className="flex flex-col justify-between border-[1.5px] border-custom-primary shadow-lg p-3 gap-5 group" href={`/program/${program.slug.current}`} key={idx}>
+      {program.images && program.images.length > 0 && (
+        <div className="relative self-center w-full overflow-hidden aspect-square xl:aspect-video group">
+          <Image src={urlForImage(program.images[0]).url()} className="object-cover w-full h-full group-hover:scale-[103%] duration-500" fill={true} alt={`program 0`} />
+        </div>
+      )}
+
+      <div className="space-y-1">
+        <Text type="title" text={program.name} />
+        <Text classes="line-clamp-3 xl:line-clamp-2 sm:line-clamp-3" type="caption" text={program.short_description} />
+      </div>
+
+      <Button type="button" text="Подробнее" size="md" />
+    </Link>
+  ))
+
   return (
     <section data-section="programs-index" className="mt-20 space-y-10 sm:space-y-7">
       <Heading type="title" classes="text-center" text="Программы" />
 
-      <div className="grid items-start grid-cols-4 gap-5 xl:grid-cols-2 sm:grid-cols-1">
-        {programs.map((program, idx) => (
-          <Link className="flex flex-col justify-between border-[1.5px] border-custom-primary shadow-lg p-3 gap-5 group" href={`/program/${program.slug.current}`} key={idx}>
-            {program.images && program.images.length > 0 && (
-              <div className="relative self-center w-full aspect-square xl:aspect-video">
-                <Image src={urlForImage(program.images[0]).url()} className="object-cover w-full h-full" fill={true} alt={`program 0`} />
-              </div>
-            )}
-
-            <div className="space-y-1">
-              <Text type="title" text={program.name} />
-              <Text classes="line-clamp-3 xl:line-clamp-2 sm:line-clamp-3" type="caption" text={program.short_description} />
-            </div>
-
-            <Button type="button" text="Подробнее" size="md" />
-          </Link>
-        ))}
-      </div>
+      <div className="grid items-start grid-cols-4 gap-5 xl:grid-cols-2 sm:grid-cols-1">{slides}</div>
     </section>
   )
 }
