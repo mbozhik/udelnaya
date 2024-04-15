74e088fa8c62da3822cd880f5c067df6d6054c41
bozzhik <bozzhik@ya.ru>
2024-04-16 01:49:20 +0300

    init `rooms` entity into `Sanity` CMS

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import promo from './promo'
 import medicine from './medicine'
 import programs from './programs'
+import rooms from './rooms'
 
-export const schemaTypes = [promo, medicine, programs]
+export const schemaTypes = [promo, medicine, programs, rooms]
---
+++ sanity/schemaTypes/rooms.ts
@@ -0,0 +1,53 @@
+import {subscribe} from 'diagnostics_channel'
+import {Rule, SchemaTypeDefinition} from 'sanity'
+
+const rooms: SchemaTypeDefinition = {
+  name: 'rooms',
+  title: 'Номера',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Название номера',
+      type: 'string',
+      validation: (rule: Rule) => rule.required(),
+    },
+    {
+      name: 'description',
+      title: 'Описание номера',
+      type: 'text',
+      validation: (rule: Rule) => rule.required(),
+    },
+    {
+      name: 'specification',
+      title: 'Спецификация номера',
+      type: 'array',
+      of: [{type: 'block'}],
+      validation: (rule: Rule) => rule.required(),
+    },
+    {
+      name: 'images',
+      title: 'Изображения',
+      type: 'array',
+      of: [{type: 'image', options: {hotspot: true}}],
+    },
+    {
+      name: 'slug',
+      title: 'Ссылка',
+      type: 'slug',
+      options: {
+        source: 'name',
+      },
+      validation: (rule: Rule) => rule.required(),
+    },
+  ],
+  preview: {
+    select: {
+      title: 'name',
+      subtitle: 'description',
+      media: 'images.0.asset',
+    },
+  },
+}
+
+export default rooms
