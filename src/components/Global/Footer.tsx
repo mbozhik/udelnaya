892c994406d64f923bdc09628db55b5eb720647c
bozzhik <bozzhik@ya.ru>
2024-04-16 17:46:40 +0300

    [2] minor edit `Footer`

--- src/components/Global/Footer.tsx
+++ src/components/Global/Footer.tsx
@@ -21,19 +21,19 @@ export default function Footer() {
   return (
     <footer className="bg-custom-light-gray pt-10 pb-5 mt-20">
       <Container padding={false}>
-        <div className="space-y-7 xl:space-y-5 sm:space-y-7">
+        <div className="space-y-5 xl:space-y-5 sm:space-y-5">
           <Button type="link" href="/" size="lg" classes="uppercase" adavanced_hover={true} text={`Цены на услуги ${currentYear}`} />
 
           <div className="space-y-2 sm:space-y-5">
             <div className="flex sm:flex-col justify-between sm:gap-2">
               {Object.values(footerContent.nav).map((link, idx) => (
-                <Link className="xl:text-xs sm:text-sm hover:text-custom-primary duration-200" href={link.href} key={idx}>
+                <Link className="xl:text-xs sm:text-sm text-custom-primary hover:text-custom-gray duration-200" href={link.href} key={idx}>
                   {link.name}
                 </Link>
               ))}
             </div>
 
-            <div className="xl:text-sm sm:text-sm flex sm:flex-col sm:text-left justify-between text-center text-custom-primary">
+            <div className="xl:text-sm sm:text-sm flex sm:flex-col sm:gap-2 sm:text-left justify-between text-center">
               <span>ООО «Санаторий Удельная»</span>
               <span>© 2024 | sanatoriyudelnaya.ru</span>
             </div>
