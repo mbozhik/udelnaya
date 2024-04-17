0dadd7d2feffcd93c5184f5a4af97e8b6645cac1
bozzhik <bozzhik@ya.ru>
2024-04-17 16:48:37 +0300

    minimize typography in `Medicine`

--- src/components/UI/Heading.tsx
+++ src/components/UI/Heading.tsx
@@ -1,16 +1,16 @@
 import {cn} from '@/lib/utils'
 
 interface Props {
-  type: 'title' | 'caption'
+  type: 'title' | 'subtitle' | 'caption'
   text: string
   classes?: string
 }
 
 const Heading: React.FC<Props> = ({type, text, classes}) => {
-  if (type === 'title') {
-    return <h1 className={`text-4xl sm:text-3xl font-medium uppercase tracking-tight ${cn(classes)}`} dangerouslySetInnerHTML={{__html: text || ''}} />
+  if (type === 'title' || type === 'subtitle') {
+    return <h1 className={`${type === 'title' ? 'text-4xl' : 'text-3xl'} sm:text-3xl font-medium uppercase tracking-tight ${cn(classes)}`} dangerouslySetInnerHTML={{__html: text || ''}} />
   } else if (type === 'caption') {
-    return <p className={`text-base xl:text-lg sm:text-base font-light ${cn(classes)}`} dangerouslySetInnerHTML={{__html: text || ''}} />
+    return <p className={`font-light ${cn(classes)}`} dangerouslySetInnerHTML={{__html: text || ''}} />
   }
 }
 
