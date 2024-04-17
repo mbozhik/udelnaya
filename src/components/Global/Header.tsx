5b885a4e36fd3d914d5fd36e6eb829d7d65b4f3b
bozzhik <bozzhik@ya.ru>
2024-04-17 16:37:20 +0300

    minimize typography in `Header`

--- src/components/Global/Header.tsx
+++ src/components/Global/Header.tsx
@@ -30,12 +30,12 @@ export default function Header() {
   return (
     <>
       <header className="fixed inset-0 z-50 flex justify-between w-full px-5 py-3 sm:py-1.5 tracking-tight sm:px-3 h-fit bg-white/90 backdrop-filter backdrop-blur-sm">
-        <div className="flex items-center gap-10">
+        <div className="flex items-center gap-7">
           <Link href="/">
-            <Image className="object-contain duration-300 s-14 xl:s-12 hover:scale-110" src={LogoImage} alt="Logo" />
+            <Image className="object-contain duration-300 s-12 xl:s-12 hover:scale-110" src={LogoImage} alt="Logo" />
           </Link>
 
-          <nav className="text-xl xl:text-base font-book space-x-7 xl:space-x-5 sm:hidden">
+          <nav className="text-lg xl:text-base font-book space-x-7 xl:space-x-5 sm:hidden">
             {Object.keys(headerContent.nav).map((key) => {
               const {name, href} = headerContent.nav[key]
               return (
@@ -48,7 +48,7 @@ export default function Header() {
         </div>
 
         <div className="flex items-center gap-7">
-          <Link className="text-xl duration-200 xl:text-base font-book sm:hidden text-custom-primary hover:text-custom-gray" href={headerContent.contacts.tel.href}>
+          <Link className="text-lg duration-200 xl:text-base font-book sm:hidden text-custom-primary hover:text-custom-gray" href={headerContent.contacts.tel.href}>
             {headerContent.contacts.tel.name}
           </Link>
 
