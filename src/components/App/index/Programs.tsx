b52ddface01d8d090dfff526dabe4012fd1ac2b7
bozzhik <bozzhik@ya.ru>
2024-04-17 16:51:57 +0300

    minimize typography in `Programs`

--- src/components/App/index/Programs.tsx
+++ src/components/App/index/Programs.tsx
@@ -45,7 +45,7 @@ const Program = async () => {
   const slides = programs.slice(sliceStart, sliceEnd).map((program, idx) => (
     <Link className="flex flex-col justify-between border-[1.5px] border-custom-primary shadow-lg p-3 gap-5 group" href={`/programs/${program.slug.current}`} key={idx}>
       <div className="relative self-center w-full overflow-hidden aspect-square xl:aspect-video group">
-        <Image className="object-cover w-full h-full group-hover:scale-[103%] duration-500" src={urlForImage(program.images[0]).url()} fill={true} sizes="25vw" alt={`program 0`} />
+        <Image className="object-cover w-full h-full group-hover:scale-[102%] duration-500" src={urlForImage(program.images[0]).url()} fill={true} sizes="25vw" alt={`program 0`} />
       </div>
 
       <div className="space-y-1">
@@ -58,7 +58,7 @@ const Program = async () => {
   ))
 
   return (
-    <section data-section="programs-index" className="space-y-10 xl:space-y-5 sm:space-y-7">
+    <section data-section="programs-index" className="space-y-7 xl:space-y-5 sm:space-y-7">
       <Heading type="title" classes="text-center" text="Программы" />
 
       <div className="grid items-start grid-cols-4 gap-5 xl:grid-cols-2 sm:grid-cols-1">{slides}</div>
