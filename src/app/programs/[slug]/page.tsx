93edf1a13fa74906f2cd6df950917ecc9045371c
bozzhik <bozzhik@ya.ru>
2024-04-23 03:50:14 +0300

    remake `/programs/[slug]` page

--- src/app/programs/[slug]/page.tsx
+++ src/app/programs/[slug]/page.tsx
@@ -14,6 +14,7 @@ interface ProgramPage {
   name: string
   duration: string
   description: any
+  type: Array<{name: string; slug: string}>
   images: Array<{asset: {url: string}}>
   pdf: {asset: {_ref: string}}
   slug: {current: string}
@@ -25,6 +26,7 @@ async function getData(slug): Promise<ProgramPage | null> {
         name,
         duration,
         description,
+        type[] -> { name, slug },
         images,
         pdf,
         slug
@@ -56,10 +58,10 @@ const ProgramPage = async ({params}) => {
     }
   }
 
-  const imagesStyles = 'relative w-full h-[50vh] xl:h-full sm:h-[50vh]'
+  const imagesStyles = 'relative w-full h-[50vh] aspect-square xl:h-full sm:h-[50vh]'
 
   return (
-    <Container width="3/4">
+    <Container width="2/3">
       <section data-index={program.slug.current} className="grid mx-auto place-items-center">
         <div className="space-y-5 group border-[1.5px] border-custom-primary shadow-lg p-5 sm:p-3">
           <div className="grid grid-cols-2 sm:grid-cols-1 gap-10 sm:gap-7 items-center">
@@ -75,9 +77,15 @@ const ProgramPage = async ({params}) => {
 
             <div className="flex flex-col gap-5 xl:py-5 sm:py-0 sm:gap-5">
               <div className="space-y-2">
+                <mark>
+                  {program.type.map((type, index) => (
+                    <span key={index}>{type.name}</span>
+                  ))}
+                </mark>
+
                 <Heading type="title" text={program.name} />
 
-                {program.duration && <mark>{program.duration}</mark>}
+                {program.duration && <h1>{program.duration}</h1>}
               </div>
 
               <div className="w-[90%] sm:w-full">
@@ -85,12 +93,12 @@ const ProgramPage = async ({params}) => {
               </div>
 
               {program.pdf ? (
-                <div className="grid grid-cols-2 gap-3 sm:grid-cols-1">
-                  <Button type="link" text="Узнать детали" size="lg" variant="secondary" adavanced_hover={true} blank={true} href={urlForFile(program.pdf.asset._ref)} />
-                  <Button type="button" text="Забронировать" size="lg" adavanced_hover={true} classes="w-full" />
+                <div className="flex gap-3">
+                  <Button type="link" text="Узнать детали" size="lg" variant="secondary" classes="w-fit" adavanced_hover={true} blank={true} href={urlForFile(program.pdf.asset._ref)} />
+                  <Button type="button" text="Забронировать" size="lg" adavanced_hover={true} classes="w-fit" />
                 </div>
               ) : (
-                <Button type="button" text="Забронировать" size="lg" adavanced_hover={true} classes="w-full" />
+                <Button type="button" text="Забронировать" size="lg" adavanced_hover={true} classes="w-fit" />
               )}
             </div>
           </div>
