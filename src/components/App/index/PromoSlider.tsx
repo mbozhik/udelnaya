73ab43ab3bc7f8acf4405a56ec4452873984cde6
bozzhik <bozzhik@ya.ru>
2024-04-23 23:18:43 +0300

    fix using `mobile_image` in `PromoSlider` child component

--- src/components/App/index/PromoSlider.tsx
+++ src/components/App/index/PromoSlider.tsx
@@ -17,19 +17,20 @@ interface SliderProps {
     title: string
     caption: string
     imageUrl: string
+    mobileImageUrl?: string
   }[]
   classes: string
 }
 
 const Slider: React.FC<SliderProps> = ({sliderData, classes}) => {
   return (
-    <Swiper data-section="promo-index" className={classes} loop={true} speed={1000} autoplay={{delay: 3000}} pagination={{clickable: true}} grabCursor={true} modules={[Pagination, Autoplay]}>
+    <Swiper data-section="promo-index" className={classes} loop={true} speed={1000} autoplay={{delay: 3000, disableOnInteraction: true}} pagination={{clickable: true}} grabCursor={true} modules={[Pagination, Autoplay]}>
       {sliderData.map((slide, index) => (
         <SwiperSlide className="relative grid place-items-center" key={index}>
           {!isMobile ? (
-            <Image quality={100} priority={true} className="absolute inset-0 block object-cover s-full" src={slide.imageUrl} width="1920" height="700" alt={`акция ${index + 1}`} /> // desktop
+            <Image quality={100} priority={true} className="absolute inset-0 block object-cover s-full" width="1920" height="700" alt={`акция ${index + 1}`} src={slide.imageUrl} />
           ) : (
-            <Image quality={100} priority={true} className="absolute inset-0 block object-cover s-full" src={slide.imageUrl} width="450" height="900" alt={`акция ${index + 1}`} />
+            <Image quality={100} priority={true} className="absolute inset-0 block object-cover s-full" width="450" height="900" alt={`акция ${index + 1}`} src={slide.mobileImageUrl || slide.imageUrl} /> // Use mobileImageUrl if available, otherwise fallback to imageUrl
           )}
 
           <div className="absolute inset-0 flex flex-col justify-center bg-black bg-opacity-10">
