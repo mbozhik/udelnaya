4bc6acadd2d89b358526848afdb8bd0cd3ad0008
max <bozzhik@ya.ru>
2024-04-27 17:50:57 +0300

    use `id` to arrange cards in `Programs` index block

--- src/components/App/index/Programs.tsx
+++ src/components/App/index/Programs.tsx
@@ -11,6 +11,7 @@ import Button from '#/UI/Button'
 interface ProgramCategories {
   name: string
   description: string
+  id: number
   image: Array<{asset: {url: string}}>
   slug: {current: string}
 }
@@ -20,6 +21,7 @@ async function getData(): Promise<ProgramCategories[]> {
     `*[_type == 'programs_category'] {
         name,
         description,
+        id,
         image,
         slug
     }`,
@@ -40,18 +42,20 @@ const ProgramCategories = async () => {
     return <mark>Произошла ошибка при получении данных!</mark>
   }
 
-  const slides = programCategories.map((category, idx) => (
-    <Link className="flex flex-col justify-between gap-4 p-3 pb-4 rounded-md xl:gap-3 shadow-card group" href={`/programs/#${category.slug.current}`} key={idx}>
-      <div className="relative self-center w-full overflow-hidden aspect-video group">
-        <Image className="object-cover w-full h-full group-hover:scale-[102%] duration-500 rounded-[4px]" src={urlForImage(category.image).url()} fill={true} sizes="25vw" alt={`category 0`} />
-      </div>
+  const slides = programCategories
+    .sort((a, b) => a.id - b.id)
+    .map((category, idx) => (
+      <Link className="flex flex-col justify-between gap-4 p-3 pb-4 rounded-md xl:gap-3 shadow-card group" href={`/programs/#${category.slug.current}`} key={idx}>
+        <div className="relative self-center w-full overflow-hidden aspect-video group">
+          <Image className="object-cover w-full h-full group-hover:scale-[102%] duration-500 rounded-[4px]" src={urlForImage(category.image).url()} fill={true} sizes="25vw" alt={category.name} />
+        </div>
 
-      <div>
-        <Text type="title" text={category.name} />
-        <Text classes="line-clamp-3" type="caption" text={category.description} />
-      </div>
-    </Link>
-  ))
+        <div>
+          <Text type="title" text={category.name} />
+          <Text classes="line-clamp-3" type="caption" text={category.description} />
+        </div>
+      </Link>
+    ))
 
   return (
     <section data-section="programs-index" className="space-y-7 xl:space-y-5">
