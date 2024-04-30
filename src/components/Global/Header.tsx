1a76c1ec6eaa034b0f17b376c80f008137e4b47b
max <bozzhik@ya.ru>
2024-04-30 16:19:01 +0300

    optimize `Nav` Global component

--- src/components/Global/Header.tsx
+++ src/components/Global/Header.tsx
@@ -52,37 +52,13 @@ export const headerData = {
       },
     },
     4: {
-      type: 'dropdown',
-      config: {
-        name: 'Программы',
-        href: '/programs/',
-        content: [
-          {
-            title: 'Alert Dialog',
-            href: '/docs/primitives/alert-dialog',
-            description: 'A modal dialog that interrupts the user with important content and expects a response.',
-          },
-          {
-            title: 'Hover Card',
-            href: '/docs/primitives/hover-card',
-            description: 'For sighted users to preview content available behind a link.',
-          },
-          {
-            title: 'Progress',
-            href: '/docs/primitives/progress',
-            description: 'Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.',
-          },
-        ],
-      },
-    },
-    44: {
       type: 'link',
       config: {
         name: 'Номера',
         href: '/rooms/',
       },
     },
-    6: {
+    5: {
       type: 'link',
       config: {
         name: 'Контакты',
--- src/components/Global/Nav.tsx
+++ src/components/Global/Nav.tsx
@@ -1,54 +1,44 @@
 'use client'
 
-import {useState} from 'react'
 import Link from 'next/link'
 
 import {headerData} from '#/Global/Header'
 
 export default function Nav() {
-  const [dropdownIndex, setDropdownIndex] = useState(null)
-
-  const handleMouseEnter = (index) => {
-    setDropdownIndex(index)
-  }
-
-  const handleMouseLeave = () => {
-    setDropdownIndex(null)
-  }
+  const navLinks = Object.keys(headerData.nav)
 
   return (
     <nav className="flex sm:grid sm:grid-cols-4 justify-center border-t-[1.5px] border-b-[1.5px] font-book border-custom-gray/70 ">
-      {Object.keys(headerData.nav).map((key, index) => {
+      {navLinks.map((key, index) => {
         const linkClasses = {
-          position: 'py-1.5 px-5 sm:px-2 sm:text-xs sm:font-normal sm:text-center cursor-pointer',
-          style: 'border-l-[1.5px] border-custom-gray/70 hover:bg-custom-dirty-white duration-300',
+          default: `py-2 px-5 sm:px-2 
+                    sm:text-xs sm:text-center cursor-pointer
+                    hover:bg-custom-dirty-white duration-300
+                    ${index === navLinks.length - 1 && 'border-r-[1.5px]'}
+                    ${index === 4 && 'sm:hidden'}
+                    `,
+          style: ' border-l-[1.5px] border-custom-gray/70 ',
         }
 
         const item = headerData.nav[key]
         if (item.type === 'dropdown') {
           return (
-            <div key={index} className={linkClasses.style} onMouseEnter={() => handleMouseEnter(index)} onMouseLeave={handleMouseLeave}>
-              <div className={`relative ${linkClasses.position}`}>{item.config.name}</div>
-              {dropdownIndex === index && (
-                <div className="absolute bg-custom-dirty-white">
-                  <ul className="grid gap-3 py-1.5 px-5 sm:px-2 -ml-1.5">
-                    {item.config.content.map((subItem, subIndex) => (
-                      <li key={subIndex} title={subItem.title}>
-                        <Link href={subItem.href}>
-                          <h1>{subItem.title}</h1>
-                        </Link>
-                      </li>
-                    ))}
-                  </ul>
-                </div>
-              )}
+            <div className="overflow-hidden group" key={index}>
+              <h1 className={`flex items-center group group-hover:bg-custom-dirty-white ${linkClasses.default} ${linkClasses.style}`}>{item.config.name}</h1>
+              <div className="hidden group-hover:block absolute bg-white shadow-md z-10 ml-[1px] peer">
+                {item.config.content.map((subItem, subIndex) => (
+                  <Link href="#" className={`block text-left border-t-[1.5px] border-custom-gray/40 ${linkClasses.default}`} key={subIndex}>
+                    {subItem.title}
+                  </Link>
+                ))}
+              </div>
             </div>
           )
         } else if (item.type === 'link') {
           return (
-            <div key={index} className={linkClasses.position + linkClasses.style}>
-              <Link href={item.config.href}>{item.config.name}</Link>
-            </div>
+            <Link href={item.config.href} className={`${linkClasses.default} ${linkClasses.style}`} key={index}>
+              {item.config.name}
+            </Link>
           )
         }
       })}
