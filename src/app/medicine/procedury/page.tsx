10bd9e7631ee21563e10cbb8ddbc1418a176c541
bozzhik <bozzhik@ya.ru>
2024-05-11 02:54:30 +0300

    refactor `/procedury` page

--- src/app/medicine/procedury/page.tsx
+++ src/app/medicine/procedury/page.tsx
@@ -4,30 +4,28 @@ import {revalidateOnTime} from '@/lib/utils'
 import Link from 'next/link'
 import Image from 'next/image'
 import {PortableText} from '@portabletext/react'
-import {PortableImage} from '#/UI/PortableImage'
 
 import Container from '#/Global/Container'
 import Heading from '#/UI/Heading'
-import Text from '#/UI/Text'
 import Questions from '##/index/Questions'
 
-interface MedicinePage {
+interface ProceduryPageProps {
   name: string
   procedures?: Array<{name: string; description: any; slug: {current: string}}>
   short_description: any
+  description: any
   image: Array<{asset: {url: string}}>
-  special_offer: boolean
   slug: {current: string}
 }
 
-async function getData(): Promise<MedicinePage | null> {
-  const data = await client.fetch<MedicinePage>(
+async function getData(): Promise<ProceduryPageProps | null> {
+  const data = await client.fetch<ProceduryPageProps>(
     `*[_type == 'medicine' && slug.current == 'procedury'][0] {
         name,
         procedures[] -> { name, description, slug },
         short_description,
+        description,
         image,
-        special_offer,
         slug
     }`,
     {},
@@ -40,14 +38,20 @@ async function getData(): Promise<MedicinePage | null> {
   return data || null
 }
 
-const ProgramPage = async () => {
-  const medicine: MedicinePage | null = await getData()
+const ProceduryPage = async () => {
+  const medicine: ProceduryPageProps | null = await getData()
 
   if (!medicine) {
     return <mark>Произошла ошибка при получении данных!</mark>
   }
 
-  const imagesStyles = 'relative col-span-2 w-full aspect-[3/2] rounded-[4px]'
+  const gridConfig = {
+    global: 'grid-cols-7',
+    image: 'col-span-3',
+    text: 'col-span-4',
+  }
+
+  const imagesStyles = `relative w-full h-full aspect-[3/2] rounded-[4px] ${gridConfig.image}`
 
   return (
     <Container width="2/3" className="space-y-20 mt-7">
@@ -55,22 +59,24 @@ const ProgramPage = async () => {
         <Heading type="title" text={medicine.name} />
 
         <section data-index={medicine.slug.current} className="mb-20 mt-14">
-          <div className="p-5 space-y-10 rounded-md sm:space-y-7 shadow-card group sm:p-3">
-            <div className="grid items-center grid-cols-6 gap-10 sm:flex sm:flex-col sm:gap-5">
+          <div className="p-5 space-y-12 rounded-md sm:space-y-7 shadow-card group sm:p-3">
+            <div className={`grid items-center ${gridConfig.global} gap-10 sm:flex sm:flex-col sm:gap-5`}>
               <div className={`relative ${imagesStyles}`}>
                 <Image quality={100} className="object-cover" src={urlForImage(medicine.image).url()} fill={true} sizes="25vw" alt={`${medicine.name}`} />
               </div>
 
-              <div className="col-span-4 pr-10 space-y-2 sm:pr-2">
-                <Text type="title" text={medicine.name} />
-
+              <div className={`${gridConfig.text} pr-10 space-y-2 sm:pr-2`}>
                 <PortableText value={medicine.short_description} />
               </div>
             </div>
 
+            <div className="pr-10 space-y-2">
+              <PortableText value={medicine.description} />
+            </div>
+
             <div className="grid grid-cols-3 gap-3 sm:grid-cols-1">
               {medicine.procedures.map((procedure, index) => (
-                <Link className="py-5 text-center text-white duration-300 rounded-md bg-custom-primary hover:bg-custom-gray" href={`/medicine/procedury/${procedure.slug.current}`} key={index}>
+                <Link className="p-5 text-center text-white duration-300 rounded-md bg-custom-primary hover:bg-custom-gray" href={`/medicine/procedury/${procedure.slug.current}`} key={index}>
                   {procedure.name}
                 </Link>
               ))}
@@ -83,4 +89,4 @@ const ProgramPage = async () => {
   )
 }
 
-export default ProgramPage
+export default ProceduryPage
